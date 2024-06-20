9afeeeda07d73771e1e839421e71943958d63fba
Jean Paul <jeanpaul.puerta@utp.edu.co>
2024-06-19 19:28:51 -0500

    Added login layout, no images yet.

--- Front - React/src/components/LogIn/Login.js
+++ Front - React/src/components/LogIn/Login.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from 'react';
+import LoginForm from './LoginForm';
+import imageLogin from './imagelogin.png'; // Asegúrate de la ruta correcta a tu imagen
 
 function Login() {
   return (
-    <div className='text-6xl text-red-700'>Login</div>
-  )
+    <div className="min-h-screen bg-gray-300 flex items-center justify-center">
+      <div className="max-w-4xl mx-auto p-8 flex flex-col md:flex-row items-center bg-gray-300 shadow-lg rounded-lg">
+        <div className="md:w-1/2 p-4 flex flex-col items-center">
+          <h1 className="text-3xl font-bold mb-2 text-shadow">INSPECCIÓN</h1>
+          <h2 className="text-xl text-blue-600 mb-6 font-bold">PREOPERACIONAL DE VEHÍCULOS</h2>
+          <LoginForm />
+        </div>
+        <div className="md:w-1/2 p-4 flex items-center justify-center">
+          <img src={imageLogin} alt="Inspección Preoperacional de Vehículos" className="max-w-full h-auto" />
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
--- Front - React/src/components/LogIn/LoginForm.js
+++ Front - React/src/components/LogIn/LoginForm.js
@@ -1,9 +1,59 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { EyeIcon, EyeOffIcon } from '@heroicons/react/solid'; 
 
 function LoginForm() {
+  const [showPassword, setShowPassword] = useState(true); // Estado para controlar la visibilidad de la contraseña
+
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword); // Cambia el estado para mostrar u ocultar la contraseña
+  };
+
   return (
-    <div>LoginForm</div>
-  )
+    <div className="p-8 rounded-lg shadow-md w-full max-w-md border-dotted border-2 border-gray-400">
+      <h3 className="text-center text-2xl font-bold mb-6">LOGIN</h3>
+      <form>
+        <div className="mb-4">
+          <label className="pl-2 text-sm block text-gray-700 italic">Correo Electrónico:</label>
+          <input
+            type="email"
+            className="w-full px-4 py-2 border rounded-full border-blue-700 focus:outline-none focus:border-gray-300"
+          />
+        </div>
+        <div className="mb-4 relative">
+          <label className="pl-2 text-sm block text-gray-700 italic">Contraseña:</label>
+          <input
+            type={showPassword ? "text" : "password"} // Mostrar texto si showPassword es true
+            className="w-full px-4 py-2 border rounded-full border-blue-700 focus:outline-none focus:border-gray-300"
+          />
+          <button
+            type="button"
+            className="absolute inset-y-0 right-0 px-3 py-2 pt-8"
+            onClick={togglePasswordVisibility}
+          >
+            {showPassword ? (
+              <EyeOffIcon className="h-6 w-6 text-gray-500 hover:text-gray-700" aria-hidden="true" />
+            ) : (
+              <EyeIcon className="h-6 w-6 text-gray-500 hover:text-gray-700" aria-hidden="true" />
+            )}
+          </button>
+        </div>
+        <div className="flex justify-between items-center mb-6">
+          <a href="/login" className="text-blue-500 text-sm italic">¿Olvidaste tu contraseña?</a>
+        </div>
+        <button
+          type="submit"
+          className="w-full bg-blue-600 text-white py-2 font-bold rounded-full hover:bg-blue-700 transition duration-200"
+        >
+          INGRESAR
+        </button>
+        <div className="text-left  mt-4">
+        <a href="/login" className="text-sm">
+        <span className="text-black">No tienes una cuenta?</span> <span className="text-blue-500 font-bold  ">Registrarse</span>
+        </a>
+        </div>
+      </form>
+    </div>
+  );
 }
 
-export default LoginForm
+export default LoginForm;
