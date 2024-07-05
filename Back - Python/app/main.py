f6f5d3daabf214897958adceb3d80f7ca689efe2
jeanpaul615 <jeanpaul.puerta@utp.edu.co>
2024-07-05 13:54:08 -0500

    allows cors

--- Back - Python/app/main.py
+++ Back - Python/app/main.py
@@ -4,11 +4,18 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+# Configuración de CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # Permitir solicitudes desde localhost:3000
+    allow_credentials=True,
+    allow_methods=["*"],  # Permitir todos los métodos (GET, POST, etc.)
+    allow_headers=["*"],  # Permitir todos los encabezados
+)
+
 app.include_router(user.router, prefix="/api/v1", tags=["users"])
 app.include_router(login.router, prefix="/api/v1", tags=["login"])
 
 @app.get("/")
 def read_root():
     return {"message": "Welcome to FastAPI"}
-
-
